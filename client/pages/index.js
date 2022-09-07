8c1d42cb197dda169879f9570feae65da7747299
hari <haridarshanofficial@gmail.com>
2022-09-07 16:24:55 +0530

    🚀 initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <button className="btn">Hello daisyUI</button>
+    </div>
+  );
+}
