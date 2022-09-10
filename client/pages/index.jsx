d9b5a44e1514264de80922c9450480caace571b7
hari <haridarshanofficial@gmail.com>
2022-09-10 21:32:10 +0530

    ci: added eslint prettier, commitlint and husky

---
+++ client/pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Create Next App</title>
+			</Head>
+			<button type='button' className='btn'>
+				Hello daisyUI
+			</button>
+		</div>
+	);
+}
